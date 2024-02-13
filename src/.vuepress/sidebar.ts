dfc812fad8fe3238df0b47326cfaedba49b748a7
Shenb <shenbourne@163.com>
2024-02-14 04:45:24 +0800

    129

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -14,7 +14,23 @@ export default sidebar({
       text: "剪藏",
       icon: "book",
       prefix: "notes/",
-      children: "structure",
+      children: [
+        {
+          text: "OpenWrt 安装教程",
+          icon: "arrow",
+          link: "OpenWrt-Install"
+        },
+        {
+          text: "VuePress-theme-hope2 搭建个人网站",
+          icon: "arrow",
+          link: "VuePress-theme-hope2-Deploy"
+        },
+        {
+          text: "使用Github+picGo搭建图床",
+          icon: "arrow",
+          link: "GitHub-and-picGo-Deploy"
+        },
+      ],
     },
     "intro",
     {
