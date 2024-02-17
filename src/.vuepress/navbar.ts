a674ab93494b39ef4b938b174fd4e239885a8bcd
Shenb <shenbourne@163.com>
2024-02-17 20:30:47 +0800

    4

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -7,7 +7,7 @@ export default navbar([
     text: "剪藏",
     icon: "pen-to-square",
     prefix: "/clips/",
-    children: ["GitHub-and-picGo-Deploy.md","Office-Tool-Plus-Tutorial.md","OpenWrt-Docker-Expansion.md","OpenWrt-Install.md","VuePress-theme-hope2-Deploy.md"],
+    children: ["Office-Tool-Plus-Tutorial.md","OpenWrt-Docker-Expansion.md","OpenWrt-Install.md","VuePress-theme-hope2-Deploy.md","GitHub-and-picGo-Deploy.md"],
   },
   {
     text: "V2 文档",
